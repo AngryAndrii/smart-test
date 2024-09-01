24b8cfd6bc8415a3c3fd6b482f069a1e688cbbc0
Andrii Chychkan <mywork1728@gmail>
2024-09-01 15:34:07 +0300

    added filters, work

--- src/App.tsx
+++ src/App.tsx
@@ -4,71 +4,98 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "./redux/store";
 // import { User } from "./types/users";
 import { fetchUsers } from "./axios/fetchUsers";
+import { Loader } from "./components/Loader";
+import {
+  flterName,
+  filterUserName,
+  filterEmail,
+  filterPhone,
+} from "./redux/usersSlice";
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
-  const { users } = useSelector((state: RootState) => state.users);
+  const {
+    users,
+    loading,
+    nameFilter,
+    usernameFilter,
+    emailFilter,
+    phoneFilter,
+  } = useSelector((state: RootState) => state.users);
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const filterResult = users.filter((user) => {
+    return (
+      user.name.toLowerCase().includes(nameFilter.toLowerCase()) &&
+      user.username.toLowerCase().includes(usernameFilter.toLowerCase()) &&
+      user.email.toLowerCase().includes(emailFilter.toLowerCase()) &&
+      user.phone.toLowerCase().includes(phoneFilter.toLowerCase())
+    );
+  });
+
   return (
     <>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>
-              <input
-                type="text"
-                placeholder="Search by name"
-                // value={nameQuery}
-                // onChange={(e) => setNameQuery(e.target.value)}
-              />
-            </th>
-            <th>
-              <input
-                type="text"
-                placeholder="Search by username"
-                // value={usernameQuery}
-                // onChange={(e) => setUsernameQuery(e.target.value)}
-              />
-            </th>
-            <th>
-              <input
-                type="text"
-                placeholder="Search by email"
-                // value={emailQuery}
-                // onChange={(e) => setEmailQuery(e.target.value)}
-              />
-            </th>
-            <th>
-              <input
-                type="text"
-                placeholder="Search by phone"
-                // value={phoneQuery}
-                // onChange={(e) => setPhoneQuery(e.target.value)}
-              />
-            </th>
-          </tr>
-          <tr>
-            <th>Name</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>{user.phone}</td>
+      {loading ? (
+        <Loader />
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>
+                <input
+                  type="text"
+                  placeholder="Search by name"
+                  value={nameFilter}
+                  onChange={(e) => dispatch(flterName(e.target.value))}
+                />
+              </th>
+              <th>
+                <input
+                  type="text"
+                  placeholder="Search by username"
+                  value={usernameFilter}
+                  onChange={(e) => dispatch(filterUserName(e.target.value))}
+                />
+              </th>
+              <th>
+                <input
+                  type="text"
+                  placeholder="Search by email"
+                  value={emailFilter}
+                  onChange={(e) => dispatch(filterEmail(e.target.value))}
+                />
+              </th>
+              <th>
+                <input
+                  type="text"
+                  placeholder="Search by phone"
+                  value={phoneFilter}
+                  onChange={(e) => dispatch(filterPhone(e.target.value))}
+                />
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            <tr>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filterResult.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 }
--- src/redux/usersSlice.ts
+++ src/redux/usersSlice.ts
@@ -1,24 +1,33 @@
-import { createSlice} from "@reduxjs/toolkit";
-import { User } from "../types/users";
+import { createSlice, PayloadAction} from "@reduxjs/toolkit";
 import { fetchUsers } from "../axios/fetchUsers";
-
-export interface UserState {
-  users: User[];
-  loading: boolean;
-  error: string | null;
-}
+import { UserState } from "./types/userState";
 
 const initialState: UserState = {
   users: [],
   loading: false,
   error: null,
+  nameFilter: '',
+  usernameFilter: '',
+  emailFilter: '',
+  phoneFilter: '',
 }
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {
-    
+    flterName: (state, action: PayloadAction<string>) => {
+      state.nameFilter = action.payload;
+    },
+    filterUserName: (state, action: PayloadAction<string>) => {
+      state.usernameFilter = action.payload;
+    },
+    filterEmail: (state, action: PayloadAction<string>) => {
+      state.emailFilter = action.payload;
+    },
+    filterPhone: (state, action: PayloadAction<string>) => {
+      state.phoneFilter = action.payload;
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchUsers.pending, (state, _) => {
@@ -36,4 +45,5 @@ const usersSlice = createSlice({
   },
 })
 
+export const { flterName, filterUserName, filterEmail, filterPhone } = usersSlice.actions;
 export const userReducer =  usersSlice.reducer;
