a6595095a8948121eb1df410c5fc060d8a2edfe5
Andrii Chychkan <mywork1728@gmail>
2024-09-01 13:35:18 +0300

    added load users from redux, work

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
-import axios from "axios";
-
-interface Address {
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: string;
-  geo: Geo;
-}
-
-interface Geo {
-  lat: string;
-  lng: string;
-}
-
-interface Company {
-  name: string;
-  catchPhrase: string;
-  bs: string;
-}
-
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: Address;
-  phone: string;
-  website: string;
-  company: Company;
-}
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "./redux/store";
+// import { User } from "./types/users";
+import { fetchUsers } from "./axios/fetchUsers";
 
 function App() {
-  const [usersList, setUsers] = useState<User[]>([]);
-  const [nameQuery, setNameQuery] = useState<string>("");
-  const [usernameQuery, setUsernameQuery] = useState<string>("");
-  const [emailQuery, setEmailQuery] = useState<string>("");
-  const [phoneQuery, setPhoneQuery] = useState<string>("");
-
-  const getUsers = async (): Promise<void> => {
-    const users = await axios.get<User[]>(
-      "https://jsonplaceholder.typicode.com/users/"
-    );
-    setUsers(users.data);
-  };
+  const dispatch = useDispatch<AppDispatch>();
+  const { users } = useSelector((state: RootState) => state.users);
 
   useEffect(() => {
-    getUsers();
-  }, []);
-
-  const filteredUsers = usersList.filter((user) => {
-    return (
-      user.name.toLowerCase().includes(nameQuery.toLowerCase()) &&
-      user.username.toLowerCase().includes(usernameQuery.toLowerCase()) &&
-      user.email.toLowerCase().includes(emailQuery.toLowerCase()) &&
-      user.phone.toLowerCase().includes(phoneQuery.toLowerCase())
-    );
-  });
+    dispatch(fetchUsers());
+  }, [dispatch]);
 
   return (
     <>
@@ -68,32 +22,32 @@ function App() {
               <input
                 type="text"
                 placeholder="Search by name"
-                value={nameQuery}
-                onChange={(e) => setNameQuery(e.target.value)}
+                // value={nameQuery}
+                // onChange={(e) => setNameQuery(e.target.value)}
               />
             </th>
             <th>
               <input
                 type="text"
                 placeholder="Search by username"
-                value={usernameQuery}
-                onChange={(e) => setUsernameQuery(e.target.value)}
+                // value={usernameQuery}
+                // onChange={(e) => setUsernameQuery(e.target.value)}
               />
             </th>
             <th>
               <input
                 type="text"
                 placeholder="Search by email"
-                value={emailQuery}
-                onChange={(e) => setEmailQuery(e.target.value)}
+                // value={emailQuery}
+                // onChange={(e) => setEmailQuery(e.target.value)}
               />
             </th>
             <th>
               <input
                 type="text"
                 placeholder="Search by phone"
-                value={phoneQuery}
-                onChange={(e) => setPhoneQuery(e.target.value)}
+                // value={phoneQuery}
+                // onChange={(e) => setPhoneQuery(e.target.value)}
               />
             </th>
           </tr>
@@ -105,7 +59,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {filteredUsers.map((user) => (
+          {users.map((user) => (
             <tr key={user.id}>
               <td>{user.name}</td>
               <td>{user.username}</td>
